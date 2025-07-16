3905b11e65c2fa3d741c4782f40df372091cae09
isakez <isaksanchez21@gmail.com>
2025-07-16 17:12:24 +0200

    sube C01 ejercicios 1 y 2

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,25 @@
+
+
+#include <unistd.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+/*
+int	main(void)
+{
+	int	num;
+	char	wrnum;
+
+	num = 3;
+	wrnum = num + 48;
+
+	write(1, &wrnum, 1);
+	write(1, " ", 1);
+	ft_ft(&num);
+	wrnum = num + 48;
+	write(1, &wrnum, 1);
+
+}
+*/ 
---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********num)
+{
+	*********num = 42;
+}
+
+/*
+int	main(void)
+{
+	int	numero;
+	char	ch;
+	int	*pt1,	**pt2,  ***pt3,	****pt4,  *****pt5,  ******pt6,	*******pt7,  ********pt8,  *********pt9;
+
+
+	numero = 2;
+	ch = numero + 48;
+	write(1, &ch, 1);
+	pt1 = &numero;
+	pt2 = &pt1;
+	pt3 = &pt2;
+	pt4 = &pt3;
+	pt5 = &pt4;
+	pt6 = &pt5;
+	pt7 = &pt6;
+	pt8 = &pt7;
+	pt9 = &pt8;
+	
+	ft_ultimate_ft(pt9);
+	ch = numero + 48;
+	write(1, &ch, 1);
+	printf("el numero es: %d ", numero);
+}
+*/
