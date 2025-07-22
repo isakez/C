aa9759defb62d0f139da1a5e0ca8466f82966c9f
isakez <isaksanchez21@gmail.com>
2025-07-22 23:20:13 +0200

    eres mia recursion

---
+++ piscina_c00/ex08/ft_printcombn.c
@@ -0,0 +1,34 @@
+
+#include <unistd.h>
+
+void	recursive_combn(char *string, int index, int n)
+{
+	if (index == n)
+	{
+		write(1, string, n);
+		if (string[0] != '9' - n + 1)
+			write(1, ", ", 2);
+	}
+
+	string[index] = string[index - 1] + 1;
+	while (string[index] <= '9')
+	{
+		recursive_combn(string, index + 1, n);
+		string[index]++;
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	char	string[11];
+	
+	string[0] = 47;
+	recursive_combn(string + 1 , 0, n);
+	
+}
+
+int	main(void)
+{
+	ft_print_combn(5);
+	return (0);
+}
